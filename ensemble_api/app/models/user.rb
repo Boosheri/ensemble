56385f045e33a5fd2101d1c81dbc87bbd6a98804
Nina Boosheri <nboosheri@gmail.com>
2019-05-22 19:43:35 -0700

    add additional user validations and add gems

--- ensemble_api/app/models/user.rb
+++ ensemble_api/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
     has_many :role_tags, as: :role_relation
-    has_many :follows dependent: :destroy
+    has many :posts, dependent: :destroy
+    has_many :follows, dependent: :destroy
+
+    has_secure_password
+
+    validates(:email, presence: true, uniqueness: true, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+
+    def full_name
+        "#{first_name} #{last_name}".strip.to_s.titleize
+      end
 end
