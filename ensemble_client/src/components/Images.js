f40eb2095a98ed1540ab7cac8a5f652a6d6e28af
Nina Boosheri <nboosheri@gmail.com>
2019-05-31 23:39:16 -0700

    add seat image

--- ensemble_client/src/components/Images.js
+++ ensemble_client/src/components/Images.js
@@ -2,10 +2,12 @@ import cam1 from "../welcome/cam.jpg";
 import cam2 from "../welcome/cam2.jpg";
 import film from "../welcome/film.jpg";
 import perform from "../welcome/perform.jpg";
+import seat from "../welcome/seat.jpg";
 
 export const images = [
   { id: 1, src: cam1, title: "cam", description: "cam" },
   { id: 2, src: film, title: "film", description: "film" },
   { id: 3, src: cam2, title: "cam2", description: "cam2" },
-  { id: 4, src: perform, title: "perform", description: "perform" }
+  { id: 4, src: perform, title: "perform", description: "perform" },
+  { id: 5, src: seat, title: "seat", description: "seat" }
 ];
