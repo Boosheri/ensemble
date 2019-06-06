619e440bcd9ce050a9ae25eae031a28443caa5a1
Nina Boosheri <nboosheri@gmail.com>
2019-06-06 15:22:12 -0700

    adjust image sizes

--- ensemble_client/src/components/Images.js
+++ ensemble_client/src/components/Images.js
@@ -5,9 +5,9 @@ import perform from "../welcome/perform.jpg";
 import seat from "../welcome/seat.jpg";
 
 export const images = [
-  { id: 1, src: cam1, title: "cam", description: "cam" },
-  { id: 2, src: film, title: "film", description: "film" },
   { id: 3, src: cam2, title: "cam2", description: "cam2" },
+  { id: 2, src: film, title: "film", description: "film" },
+  { id: 5, src: seat, title: "seat", description: "seat" },
   { id: 4, src: perform, title: "perform", description: "perform" },
-  { id: 5, src: seat, title: "seat", description: "seat" }
+  { id: 1, src: cam1, title: "cam", description: "cam" }
 ];
